1cac24794450e450199070d985381da44a22dfc5
Christopher Walker <iamchristopherwalker@gmail.com>
2020-02-18 15:00:14 -0800

    Atom plugin to perform "Select Inside Brackets" by double-clicking brackets

---
+++ spec/dbl-click-select-inside-brackets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DblClickSelectInsideBracketsView from '../lib/dbl-click-select-inside-brackets-view';
+
+describe('DblClickSelectInsideBracketsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
